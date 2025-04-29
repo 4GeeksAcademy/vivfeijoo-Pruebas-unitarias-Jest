1f7da2dc63c5771efca9d735f9f6c1116c36b079
Viviana Feijoo <vivfeijoo@gmail.com>
2025-04-29 22:36:26 +0000

    m

---
+++ app.js
@@ -0,0 +1,16 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+// Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
+module.exports = { fromEuroToDollar };
